6893be6eaaa43fec271b13775b8f338332a6b050
rodolfobruschi <bruschikc@gmail.com>
2024-12-26 18:09:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao jogo do número secreto.');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai! Você descobriu o número secreto (5)')
+}
+
